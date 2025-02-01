222d1506e16b25e1e93f385a3068883aacdb6845
uykgnuoy <uykgnuoy@gmail.com>
2025-02-01 23:11:11 +0900

    121

---
+++ public/javaScript/ImagePopup.js
@@ -0,0 +1,7 @@
+function openImagePopup() {
+	document.getElementById('image-popup').style.display = 'flex';
+}
+
+function closeImagePopup() {
+	document.getElementById('image-popup').style.display = 'none';
+}
