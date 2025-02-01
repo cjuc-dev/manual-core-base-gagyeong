45705b1cb689d32130b6be55674275d8111f9c18
uykgnuoy <uykgnuoy@gmail.com>
2025-02-01 10:30:30 +0900

    108

--- public/javaScript/homeIcon.js
+++ public/javaScript/homeIcon.js
@@ -4,10 +4,16 @@ document.addEventListener('DOMContentLoaded', function () {
 	if (homeIcon) {
 		homeIcon.addEventListener('mousedown', function () {
 			this.style.transform = 'none';
+			this.style.transition = 'none';
 		});
 
 		homeIcon.addEventListener('mouseup', function () {
 			this.style.transform = '';
+			this.style.transition = 'transform 0.3s ease-in-out';
+		});
+
+		homeIcon.addEventListener('click', function () {
+			this.style.transform = 'none';
 		});
 	}
 });
