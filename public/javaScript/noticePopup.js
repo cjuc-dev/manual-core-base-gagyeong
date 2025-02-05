383b2373535ad6b0309992b648b3871daf140e76
uykgnuoy <uykgnuoy@gmail.com>
2025-02-05 23:41:17 +0900

    155

--- public/javaScript/noticePopup.js
+++ public/javaScript/noticePopup.js
@@ -5,7 +5,7 @@ window.onload = function () {
 
 function openNoticePopup() {
 	let popupWidth = 500; // 팝업 창 너비
-	let popupHeight = 400; // 팝업 창 높이
+	let popupHeight = 550; // 팝업 창 높이
 	let left = (screen.width - popupWidth) / 2;
 	let top = (screen.height - popupHeight) / 2;
 
